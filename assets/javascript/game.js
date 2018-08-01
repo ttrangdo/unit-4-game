a47301a6e0b48becd49456a07ec75ec7c5cd125f
Trang Do <trangdo@Trangs-MBP.attlocal.net>
2018-07-31 21:52:32 -0500

    fixed alert buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,9 +13,8 @@
 var counter = 0;
 var wins = 0;
 var losses = 0;
+var gameOver = false;
 
-// target number for the round
-var randomNumber = [];
 //  * 120 = returns integer from 0 to 119
 // (Math.random()*(max-min+1)+min);
 var randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
@@ -24,69 +23,110 @@ $("#random-number").text(randomNumber);
 // display randomNumber
     $("#random-number").html(randomNumber);
 
+// display wins
+$("#wins").html("WINS: " + wins);
+// display losses
+$("#losses").html("LOSSES: " + losses);
+// 
+$("#counter").html("COUNTER: " + counter);
+
 // create gems with different/unique number values
 
 var diamond = Math.floor(Math.random() * 12) + 1;
 var ruby = Math.floor(Math.random() * 12) + 1;
 var topaz = Math.floor(Math.random() * 12) + 1;
 var emerald = Math.floor(Math.random() * 12) + 1;
-// log in console so player can keep track of which value each gem stores
-
-console.log("Diamond value: " + diamond);
-console.log("Ruby value: " + ruby);
-console.log("Emerald value: " + topaz);
-console.log("Sapphire value: " + emerald);
 
 // click event applies to every gem on the page
 
 $("#diamond").on("click", function () {
+    if(gameOver === true) {
+        return;
+    }
     counter = counter + diamond;
     $("#counter").text(counter);
-    console.log("Counter: " + counter);
     updateCounter();
+    $("#counter").html(counter);
 })
 
 $("#ruby").on("click", function () {
+    if(gameOver === true) {
+        return;
+    }
     counter = counter + ruby;
     $("#counter").text(counter);
-    console.log("Counter: " + counter);
     updateCounter();
+    
 })
 
 $("#emerald").on("click", function () {
+    if(gameOver === true) {
+        return;
+    }
     counter = counter + emerald;
     $("#counter").text(counter);
-    console.log("Counter: " + counter);
     updateCounter();
 })
 
-$("#sapphire").on("click", function () {
-    counter = counter + sapphire;
+$("#topaz").on("click", function () {
+    if(gameOver === true) {
+        return;
+    }
+    counter = counter + topaz;
     $("#counter").text(counter);
-    console.log("Counter: " + counter);
     updateCounter();
 })
 
 
-// function for update counter(score) and win/loss logic
+// function for update counter and win/loss logic
 
 function updateCounter() {
+    console.log(counter);
+    $("#counter").text(counter);
     if (counter === randomNumber) { // If the counter equals randomNumber, add 1 to wins.
         wins++;
         $("#wins").text("Wins: " + wins);
         $("#result").text("YOU WON!!!!");
-        resetGame();
-// If score goes over the randomNumber, add 1 to losses.
-    } else if (score !== 0 && score > randomNumber) { 
+        counter = 0;
+        gameOver = true;
+// If counter goes over the randomNumber, add 1 to losses.
+    } else if (counter !== 0 && counter > randomNumber) { 
         losses++;
         $("#losses").text("Losses: " + losses);
         $("#result").text("YOU LOST!!!");
-        resetGame();
+        counter = 0;
+        gameOver = true;
     }
+
 }
 
 function resetGame() {
-    document.location.href = " ";
+    // reset counter
+    counter = 0;
 
+    // reset gameover
+    gameOver = false;
+
+    //  * 120 = returns integer from 0 to 119
+    // (Math.random()*(max-min+1)+min);
+    randomNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    $("#random-number").text(randomNumber);
+
+    // remove win/lose text
+    $("#result").text("");
+
+    // show counter 
+    $("#counter").text(counter);
+
+    // display randomNumber
+    $("#random-number").html(randomNumber);
+    
+    // reset gems with different/unique number values
+    diamond = Math.floor(Math.random() * 12) + 1;
+    ruby = Math.floor(Math.random() * 12) + 1;
+    topaz = Math.floor(Math.random() * 12) + 1;
+    emerald = Math.floor(Math.random() * 12) + 1;
+
+        
 }
    
